99c9df88914459e26fbfd7749bd0fcf803b39f71
Jasim Atiyeh <jasimatiyeh@gmail.com>
2021-12-12 22:47:37 -0600

    Add schema for houses and users documents

--- pages/index.js
+++ pages/index.js
@@ -5,3 +5,31 @@ import Image from "next/image";
 export default function Home() {
   return <div></div>;
 }
+
+// SCHEMA
+
+// houses
+// {
+//   _id: ObjectId
+//   leasePayment: number,
+//   leaseStart: string,
+//   leaseEnd: string,
+//   bed: number,
+//   bath: number,
+//   squareFeet: number,
+//   address: string,
+//   zestimate: number,
+//   purchasePrice: number,
+//   paymentOnHouse: number,
+//   taxPrice: number,
+//   insurancePrice: number,
+//   yearBuilt: number,
+//   owner: string[]
+//   }
+
+// users
+// {
+//   _id: ObjectId,
+//   username: string,
+//   password: string
+// }
