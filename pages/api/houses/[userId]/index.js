cf299aff94736e5a648a1c02e7900baf524250bf
Jasim Atiyeh <jasimatiyeh@gmail.com>
2021-12-12 22:25:38 -0600

    Add endpoints for users and houses

---
+++ pages/api/houses/[userId]/index.js
@@ -0,0 +1,26 @@
+import { ObjectID } from "bson";
+import { mongoClient } from "../../../../services/api-services";
+
+// GET /houses
+// Index for getting all houses
+// todo: Would be better to have userId stored in state and only present that users houses
+export default async function handler(req, res) {
+  const db = process.env.DB;
+  const client = mongoClient();
+
+  await client.connect();
+  switch (req.method) {
+    case "GET":
+      const collection = client.db(db).collection("houses");
+      const houses = await collection
+        .find({ owners: new ObjectID(req.query.userId) })
+        .toArray();
+
+      res.status(200).json(houses);
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+  client.close();
+}
---
+++ pages/api/users/[...loginInfo].js
@@ -0,0 +1,41 @@
+import { mongoClient } from "../../../services/api-services";
+
+// GET /users/[username]/[password]
+// Returns specific user for login
+export default async function handler(req, res) {
+  const db = process.env.DB;
+  const client = mongoClient();
+
+  const requestUser = {
+    username: req.query.loginInfo[0],
+    password: req.query.loginInfo[1],
+  };
+
+  if (!query.loginInfo[1]) {
+    res.status(400).end("Password missing");
+  }
+
+  await client.connect();
+  switch (req.method) {
+    case "GET":
+      const collection = client.db(db).collection("users");
+      const user = await collection.findOne({
+        username: requestUser.username,
+        password: requestUser.password,
+      });
+
+      if (user) {
+        // User needs to be added to state??
+        res.status(200).json({ id: user._id, username: user.username });
+        break;
+      } else {
+        res.status(404).end("User not found");
+      }
+
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+  client.close();
+}
