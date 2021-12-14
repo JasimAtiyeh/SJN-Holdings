c2bb4c60b6abc68b5d5e98822fbe4d482958c51d
Jasim Atiyeh <jasimatiyeh@gmail.com>
2021-12-13 22:54:35 -0600

    New route for login api

--- pages/api/users/login/[...loginInfo].js
+++ pages/api/users/login/[...loginInfo].js
@@ -1,27 +1,19 @@
-import { mongoClient } from "../../../services/api-services";
+import { mongoClient } from "../../../../services/api-services";
 
-// GET /users/[username]/[password]
+// GET /users/login/[username]/[password]
 // Returns specific user for login
 export default async function handler(req, res) {
   const db = process.env.DB;
   const client = mongoClient();
 
-  const requestUser = {
-    username: req.query.loginInfo[0],
-    password: req.query.loginInfo[1],
-  };
-
-  if (!query.loginInfo[1]) {
-    res.status(400).end("Password missing");
-  }
-
   await client.connect();
   switch (req.method) {
     case "GET":
       const collection = client.db(db).collection("users");
+      // Username and password are in the request query
       const user = await collection.findOne({
-        username: requestUser.username,
-        password: requestUser.password,
+        username: req.query.loginInfo[0],
+        password: req.query.loginInfo[1],
       });
 
       if (user) {
@@ -29,9 +21,10 @@ export default async function handler(req, res) {
         res.status(200).json({ id: user._id, username: user.username });
         break;
       } else {
-        res.status(404).end("User not found");
+        res
+          .status(404)
+          .end("User not found. Provide correct username and password.");
       }
-
       break;
     default:
       res.setHeader("Allow", ["GET"]);
