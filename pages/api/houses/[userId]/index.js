4f2d53997b89d1e528422d848336792872fb7889
Jasim Atiyeh <jasimatiyeh@gmail.com>
2021-12-13 22:55:02 -0600

    Move post route

--- pages/api/houses/[userId]/index.js
+++ pages/api/houses/[userId]/index.js
@@ -1,7 +1,7 @@
 import { ObjectID } from "bson";
 import { mongoClient } from "../../../../services/api-services";
 
-// GET /houses
+// GET /houses/[userId]
 // Index for getting all houses
 // todo: Get userId from state and pass in with request
 export default async function handler(req, res) {
@@ -9,17 +9,29 @@ export default async function handler(req, res) {
   const client = mongoClient();
 
   await client.connect();
+  const collection = client.db(db).collection("houses");
+
   switch (req.method) {
     case "GET":
-      const collection = client.db(db).collection("houses");
       const houses = await collection
         .find({ owners: new ObjectID(req.query.userId) })
         .toArray();
 
-      res.status(200).json(houses);
+      if (houses.length !== 0) {
+        res.status(200).json(houses);
+      } else {
+        res.status(404).end("No houses for given user");
+      }
+
+      break;
+    case "POST":
+      // Changing ownerId to BSON ObjectID here so that Mongo can find it
+      req.body.owners = req.body.owners.map((owner) => new ObjectID(owner));
+      await collection.insertOne(req.body);
+      res.status(201).end();
       break;
     default:
-      res.setHeader("Allow", ["GET"]);
+      res.setHeader("Allow", ["GET", "POST"]);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
   client.close();
