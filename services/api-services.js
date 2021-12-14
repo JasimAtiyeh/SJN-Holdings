8586de75b9c27064ec577e83171ea3052a259046
Jasim Atiyeh <jasimatiyeh@gmail.com>
2021-12-13 22:54:03 -0600

    Update mongo uri

--- services/api-services.js
+++ services/api-services.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 export const mongoClient = () => {
   const mongoUsername = process.env.USERNAME;
   const mongoPassword = process.env.PASSWORD;
-  const uri = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.mejsw.mongodb.net/users?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.mejsw.mongodb.net/atiyeh-llc?retryWrites=true&w=majority`;
 
   return new MongoClient(uri);
 };
