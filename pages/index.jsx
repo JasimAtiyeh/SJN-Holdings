3ab4810a925e03ec4b8298b88eda31ebb8e9757d
Jasim Atiyeh <jasimatiyeh@gmail.com>
2021-12-13 22:56:35 -0600

    Change to jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 // Main page where all of the house data will be stored
 export default function Home() {
+  const router = useRouter();
+  const userId = localStorage.getItem("userId");
+  if (!userId) {
+    router.push("/");
+  }
   return <div></div>;
 }
 
