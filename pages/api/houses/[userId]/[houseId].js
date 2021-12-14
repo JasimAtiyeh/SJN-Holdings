910ee20d452a06f67922df882391e43d94de69e5
Jasim Atiyeh <jasimatiyeh@gmail.com>
2021-12-13 22:55:53 -0600

    Move post route. Add response ends.

--- pages/api/houses/[userId]/[houseId].js
+++ pages/api/houses/[userId]/[houseId].js
@@ -1,10 +1,9 @@
 import { ObjectID } from "bson";
 import { mongoClient } from "../../../../services/api-services";
 
-// GET /houses/[houseId]
+// GET /houses/[userId]/[houseId]
 // Returns all houses for specfic user
 export default async function handler(req, res) {
-  console.log(req.query);
   const db = process.env.DB;
   const client = mongoClient();
   const collection = client.db(db).collection("houses");
@@ -17,28 +16,21 @@ export default async function handler(req, res) {
   };
 
   await client.connect();
-  switch (method) {
-    // todo Chain promises to all requests
+  switch (req.method) {
     case "GET":
       const house = await collection.findOne(mongoFilter);
       res.status(200).json(house);
       break;
-    case "POST":
-      // todo Not sure what the request body will look like
-      await collection.insertOne(req.body);
-      res.status(201);
-      break;
     case "PATCH":
-      // todo Not sure what the request body will look like
-      await collection.updateOne(mongoFilter, req.body);
-      res.status(204);
+      await collection.updateOne(mongoFilter, { $set: req.body });
+      res.status(204).end();
       break;
     case "DELETE":
       await collection.deleteOne(mongoFilter);
-      res.status(204);
+      res.status(204).end();
       break;
     default:
-      res.setHeader("Allow", ["GET"]);
+      res.setHeader("Allow", ["GET", "PATCH", "DELETE"]);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
   client.close();
